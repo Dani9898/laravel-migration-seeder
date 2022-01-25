26e3dbe1d43a1c4a0ef6e176bffc2ec0d6cdc5b5
Dani9898 <danieleoliva98@gmail.com>
2022-01-25 16:21:48 +0100

    definire migration: artist, album, song

---
+++ database/migrations/2022_01_25_150140_create_artists_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateArtistsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('artists', function (Blueprint $table) {
+            $table->id();
+            $table -> string('name');
+            $table -> string('surname');
+            $table -> date('date_of_birth');
+            $table -> text('biografia');
+            $table -> string('year_of_debut');
+            $table -> string('album_of_debut');
+            $table -> string('vocal_style');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('artists');
+    }
+}
---
+++ database/migrations/2022_01_25_150154_create_albums_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAlbumsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('albums', function (Blueprint $table) {
+            $table->id();
+            $table -> string('title');
+            $table -> date('date_of_pubblication');
+            $table -> tinyInteger('sales');
+            $table -> tinyInteger('numer_of_tracks');
+            $table -> string('total_duration');
+            $table -> string('vocal_style');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('albums');
+    }
+}
